1759447e3286fe405cbea95fe733868708997921
Thanatip-iceJ <p.thanatip1@gmail.com>
2023-08-11 09:28:14 +0700

    Lab02-all-done!!

--- 02-Loop/06-ShoppingCart/main.js
+++ 02-Loop/06-ShoppingCart/main.js
@@ -8,15 +8,22 @@ let discount = prompt('Enter % of discount')
 // }
 
 let product = {name, amount, price, discount}
+// let product2 = {name: 'pc', amount: 5, price: 200, discount: 0}
 
 const truePrice = obj => {
     let finalPrice = 0
-    if(+obj[discount == 0]) delete obj.discount;
-    if (!'discount' in obj) {
-        finalPrice += (+obj[price] * +obj[amount])
+    if(+obj['discount'] == undefined  || obj['discount'] <= 0) {
+        delete obj['discount']
+    }
+    if (!obj['discount']){
+        finalPrice = (+obj['price'] * +obj['amount'])
     } else {
-        finalPrice += (+obj[price] * +obj[amount]) - (+obj[discount] / 10)
+        finalPrice = (+obj['price'] * +obj['amount']) * (1-(+obj['discount'] / 100))
     }
     return finalPrice;
 }
+console.log(product.discount)
+truePrice(product)
+
+
 
